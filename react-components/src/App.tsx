42a70677a4219ae5e1ab9e6c4e38536a02a2bf67
Alibek Zhulmurzin <a.zhulmurzin@gmail.com>
2022-09-20 20:11:55 +0500

    feat: add react-router and 3 pages: main, about and 404

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from 'pages/Main';
+import { Pages } from './constants';
+import AboutUs from 'pages/AboutUs';
+import PageNotFound from 'pages/PageNotFound';
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={Pages.main} element={<Main />} />
+        <Route path={Pages.aboutUs} element={<AboutUs />} />
+        <Route path={Pages.pageNotFound} element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ react-components/src/constants.ts
@@ -0,0 +1,5 @@
+export enum Pages {
+  main = '/',
+  aboutUs = 'about',
+  pageNotFound = '/*',
+}
