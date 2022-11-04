44b268c16f5869330d80c4cbddf8880b8098e19e
Alibek Zhulmurzin <a.zhulmurzin@gmail.com>
2022-11-04 19:15:00 +0500

    refactor: replace React.Context with Redux state in CharacterInfo.tsx

--- react-redux/src/pages/CharacterInfo/CharacterInfo.tsx
+++ react-redux/src/pages/CharacterInfo/CharacterInfo.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from 'components/templates/Header';
 import CenteredContent from 'components/atoms/CenteredContent';
-import AppContext from 'core/AppContext';
 import CharacterInformation from 'components/molecules/CharacterInformation';
 import { Pages } from 'core/enums';
 import Link from 'components/atoms/Link';
 import { Character } from 'core/interfaces/others';
+import { useAppSelector } from 'app/hooks';
 
 const StyledLink = styled(Link)`
   margin-left: 2rem;
@@ -15,7 +15,7 @@ const StyledLink = styled(Link)`
 
 const CharacterInfo = () => {
   const { id } = useParams();
-  const { characters } = useContext(AppContext);
+  const { characters } = useAppSelector((state) => state.app);
   const currentCharacter = characters.find((item) => {
     return item.id === Number(id);
   });
